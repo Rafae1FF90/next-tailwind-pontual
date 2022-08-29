7eaa997fcf3801e82b3e6be19b20b57350d22d7e
Rafae1FF90 <rafae1@live.dk>
2022-08-29 18:20:12 -0300

    Update CartHandler

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,17 +1,32 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useContext } from 'react';
 import Layout from '../../components/Layout';
 import data from '../../utils/data';
+import { Store } from '../../utils/Store';
 
 export default function ProductScreen() {
+  const { state, dispatch } = useContext(Store);
+
   const { query } = useRouter();
   const { slug } = query;
   const product = data.products.find((x) => x.slug === slug);
   if (!product) {
     return <div>Produto Não Encontrado.</div>;
   }
+
+  const addToCartHandler = () => {
+    const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+
+    if (product.countInStock < quantity) {
+      alert('Desculpe. A quantidade solicitada está indisponível.');
+      return;
+    }
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+  };
+
   return (
     <Layout title={product.name}>
       <div className="py-2">
@@ -52,7 +67,10 @@ export default function ProductScreen() {
                 {product.countInStock > 0 ? 'Disponível' : 'Indisponível'}
               </div>
             </div>
-            <button className="primary-button w-full">
+            <button
+              className="primary-button w-full"
+              onClick={addToCartHandler}
+            >
               Adicionar ao carrinho
             </button>
           </div>
